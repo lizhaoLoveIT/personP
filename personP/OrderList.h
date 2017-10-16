588ad5d9057f763ec3672e36fcf973ff6f3bae84
李朝 <liz@mycjj.com>
2017-10-16 11:15:39 +0800

    1

---
+++ personP/OrderList.h
@@ -0,0 +1,12 @@
+//
+//  OrderList.h
+//  CJJ
+//
+//  Created by ZBin on 16/3/23.
+//  Copyright © 2016年 cjj. All rights reserved.
+//
+
+
+@interface OrderList : BaseModel
+
+@end
---
+++ personP/OrderList.m
@@ -0,0 +1,13 @@
+//
+//  OrderList.m
+//  CJJ
+//
+//  Created by ZBin on 16/3/23.
+//  Copyright © 2016年 cjj. All rights reserved.
+//
+
+#import "OrderList.h"
+
+@implementation OrderList
+
+@end
